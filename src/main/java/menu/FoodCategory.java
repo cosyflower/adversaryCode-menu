e3c499210a2c644b0beed585255c3bc89770b422
sunghun <nokdonwithkj211@gmail.com>
2023-12-08 20:06:34 +0900

    feat(FoodCategory): 음식의 카테고리를 Enum으로 추가

--- src/main/java/menu/FoodCategory.java
+++ src/main/java/menu/FoodCategory.java
@@ -1,2 +1,19 @@
-package menu;public enum FoodCategory {
+package menu;
+
+public enum FoodCategory {
+    // 1이면 일식, 2면 한식, 3이면 중식, 4면 아시안, 5면 양식
+    JAPANESE_FOOD(1, "일식"),
+    KOREAN_FOOD(2, "한식"),
+    CHINESE_FOOD(3, "중식"),
+    ASIAN_FOOD(4, "아시안"),
+    WESTERN_FOOD(5, "양식")
+    ;
+
+    private final int foodSequence;
+    private final String foodType;
+
+    FoodCategory(int foodSequence, String foodType) {
+        this.foodSequence = foodSequence;
+        this.foodType = foodType;
+    }
 }
