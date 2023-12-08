8a6a2090b945eb154a3ed03187840d767c6c6b1e
sunghun <nokdonwithkj211@gmail.com>
2023-12-08 21:01:22 +0900

    feat(RandomNumberGenerator): 랜덤 수를 생성하는 역할을 수행하는 인터페이스 추가

---
+++ src/main/java/menu/NumberInRangeGenerator.java
@@ -0,0 +1,13 @@
+package menu;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class NumberInRangeGenerator implements RandomNumberGenerator {
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 5;
+
+    @Override
+    public int generateRandomNumber() {
+        return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+    }
+}
---
+++ src/main/java/menu/RandomNumberGenerator.java
@@ -0,0 +1,5 @@
+package menu;
+
+public interface RandomNumberGenerator {
+    int generateRandomNumber();
+}
