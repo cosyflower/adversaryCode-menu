e7fa40daf82172a7db27fd6d22ad10165ce26335
sunghun <nokdonwithkj211@gmail.com>
2023-12-09 09:04:27 +0900

    feat(view): view 관련 클래스 추가

---
+++ src/main/java/menu/view/ErrorView.java
@@ -0,0 +1,5 @@
+package menu.view;
+
+public class ErrorView {
+    private static final String ERROR_PREFIX = "[ERROR]";
+}
---
+++ src/main/java/menu/view/ViewFormat.java
@@ -0,0 +1,5 @@
+package menu.view;
+
+public class ViewFormat {
+    public static final String MENU_RECOMMENDATION_FORMAT = "[ %s | %s | %s | %s | %s | %s ]";
+}
