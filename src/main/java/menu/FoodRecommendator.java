4f8ac2e3debc6ba2af4524873dd50fe7db39a4dd
sunghun <nokdonwithkj211@gmail.com>
2023-12-09 09:11:16 +0900

    feat(FoodRecommendator): 초기에 음식들을 모두 저장하는 메서드를 추가

--- src/main/java/menu/FoodRecommendator.java
+++ src/main/java/menu/FoodRecommendator.java
@@ -1,5 +1,7 @@
 package menu;
 
+import menu.initialization.Initialization;
+import menu.repository.FoodRepository;
 import menu.view.InputView;
 import menu.view.OutputView;
 
@@ -13,17 +15,31 @@ public class FoodRecommendator {
     }
 
     public void run() {
-        setAllFoods();
-        recommendFoods();
+        FoodRepository foodRepository = setAllFoods();
+        recommendFoods(foodRepository);
         terminate();
     }
 
-    private void setAllFoods() {
+    private FoodRepository setAllFoods() {
         // Foods 정보를 저장 먼저 하고
+        FoodRepository foodRepository = Initialization.setAllMenus();
+        return foodRepository;
     }
 
-    private void recommendFoods() {
+    private void recommendFoods(FoodRepository foodRepository) {
         // 핵심 로직을 수행한다 (입력을 받고, 메뉴를 추천한다
+
+        // 1. 코치를 입력 받는다
+
+        // 2. 각 코치별 먹지 못하는 음식을 등록한다
+        // ---- 코치 관련 정보를  입력 받는다
+
+        // 코치들을 형성한다
+
+        // RandomGenerator -> WeekendCategories 를 먼저 형성한다
+        // RandomGenerator -> RecommendedFoods에 추가한다
+
+        // 결과를 출력한다
     }
 
     private void terminate() {
